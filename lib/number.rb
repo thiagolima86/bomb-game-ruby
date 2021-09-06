4770da3dd0ae312edd1e832710728789ce224e2b
Thiago <thiagolima86@gmail.com>
2021-09-06 14:41:35 -0300

    Create numbers

---
+++ lib/number.rb
@@ -0,0 +1,44 @@
+class Number
+  def initialize(size)
+    @size = size
+  end
+
+  def create
+    @number = ENV['env'] == 'test' ? create_to_test : create_rand
+    self
+  end
+
+  def generate
+    number
+  end
+
+  def reveal
+    number.join
+  end
+
+  def locked
+    value = ''
+    size.times{ value += '*' }
+    value
+  end
+
+  def eq?(value)
+    value == reveal
+  end
+
+  private
+
+  attr_reader :size, :number
+
+  def create_rand
+    number = []
+    size.times { |count| number.push(rand((count.zero? ? 1 : 0)..9)) }
+    number
+  end
+
+  def create_to_test
+    number = []
+    1.upto(size) { |count| number.push(count) }
+    number
+  end
+end
---
+++ spec/lib/number.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.describe Number do
+  subject { described_class.new(size).create }
+  context "when number size is 5" do
+    let(:size) { 5 }
+    it { expect(subject.generate).to eq [1, 2, 3, 4, 5] }
+    it { expect(subject.reveal).to eq 12345.to_s }
+    it { expect(subject.locked).to eq "*****" }
+  end
+
+  context 'when verify number' do
+    let(:size) { 3 }
+
+    it 'with value correct' do
+      expect(subject.eq?('123')).to be_truthy
+    end
+
+    it 'with value incorrect' do
+      expect(subject.eq?('124')).to be_falsey
+    end
+  end
+end
